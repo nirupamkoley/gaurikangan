8e3f3b77df5fdb84647beab1edabb38ec40ec786
Nirupam Koley <143987217+nirupamkoley@users.noreply.github.com>
2025-08-12 17:56:10 +0530

    Update ESLint configuration to disable specific warnings for improved development experience

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// Start by extending the default Next.js core-vitals rules
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    // Custom rule settings
+    rules: {
+      // Disable exhaustive-deps warning for useEffect hook
+      "react-hooks/exhaustive-deps": "off", // You can switch this to "warn" or "error" if needed
+
+      // Disable the warning for using <img> tag instead of <Image> component
+      "@next/next/no-img-element": "off", // Turn off the warning for <img> elements
+
+      // Disable the warning for using <a> tag for internal navigation
+      "@next/next/no-html-link-for-pages": "off", // Turn off the warning for <a> navigation
+    },
+  },
+];
 
 export default eslintConfig;
